621118335c0596ea2351e88deb1be12badda87bf
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-04-21 19:26:30 -0700

    add more tests

--- utils/Game.test.js
+++ utils/Game.test.js
@@ -22,11 +22,20 @@ describe("test the game", () => {
     expect(game.describePath({ X: 0, Y: 4 }, { X: 2, Y: 4 })).toEqual([
       { X: 0, Y: 4 }, { X: 1, Y: 4 }, { X: 2, Y: 4 },
     ]);
+    expect(game.describePath({ X: 0, Y: 2 }, { X: 3, Y: 2 })).toEqual([
+      { X: 0, Y: 2 }, { X: 1, Y: 2 }, { X: 2, Y: 2 }, { X: 3, Y: 2 },
+    ]);
+    expect(game.describePath({ X: 0, Y: 0 }, { X: 0, Y: 0 })).toEqual([
+      { X: 0, Y: 0 },
+    ]);
   });
 
   test("calculate a valid vertical line", () => {
     expect(game.describePath({ X: 0, Y: 4 }, { X: 0, Y: 0 })).toEqual([
       { X: 0, Y: 0 }, { X: 0, Y: 1 }, { X: 0, Y: 2 }, { X: 0, Y: 3 }, { X: 0, Y: 4 },
+    ]);
+    expect(game.describePath({ X: 2, Y: 1 }, { X: 2, Y: 2 })).toEqual([
+      { X: 2, Y: 1 }, { X: 2, Y: 2 },
     ]);
   });
 
