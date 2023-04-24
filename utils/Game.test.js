ba8f41a9452685bbef2358f44e1bf288c77feba3
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-04-23 19:30:13 -0700

    update tests to use strings

--- utils/Game.test.js
+++ utils/Game.test.js
@@ -1,4 +1,5 @@
 const { describe, test, expect, afterEach } = require('@jest/globals');
+const Point = require('./Point');
 const { makePayload, gameOver } = require('./generatePayload');
 const Game = require('./Game');
 
@@ -22,81 +23,80 @@ describe("test each individual method", () => {
 
 
   test("calculate a valid horizontal line", () => {
-    expect(game.describePath({ x: 0, y: 4 }, { x: 2, y: 4 })).toEqual([
-      { x: 0, y: 4 }, { x: 1, y: 4 }, { x: 2, y: 4 },
+    expect(game.describePath("04", "24")).toEqual([
+      "04", "14", "24",
     ]);
-    expect(game.describePath({ x: 0, y: 2 }, { x: 3, y: 2 })).toEqual([
-      { x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }, { x: 3, y: 2 },
+    expect(game.describePath("02", "32")).toEqual([
+      "02", "12", "22", "32",
     ]);
-    expect(game.describePath({ x: 0, y: 0 }, { x: 0, y: 0 })).toEqual([
-      { x: 0, y: 0 },
+    expect(game.describePath("00", "00")).toEqual([
+      "00",
     ]);
   });
 
   test("calculate a valid vertical line", () => {
-    expect(game.describePath({ x: 0, y: 4 }, { x: 0, y: 0 })).toEqual([
-      { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 },
+    expect(game.describePath("04", "00")).toEqual([
+      "00", "01", "02", "03", "04",
     ]);
-    expect(game.describePath({ x: 2, y: 1 }, { x: 2, y: 2 })).toEqual([
-      { x: 2, y: 1 }, { x: 2, y: 2 },
+    expect(game.describePath("21", "22")).toEqual([
+      "21", "22",
     ]);
   });
 
   test("calculate a valid diagonal line, negative slope", () => {
-    expect(game.describePath({ x: 0, y: 2 }, { x: 2, y: 0 })).toEqual([
-      { x: 0, y: 2 }, { x: 1, y: 1 }, { x: 2, y: 0 },
+    expect(game.describePath("02", "20")).toEqual([
+      "02", "11", "20",
     ]);
-    expect(game.describePath({ x: 2, y: 0 }, { x: 0, y: 2 })).toEqual([
-      { x: 0, y: 2 }, { x: 1, y: 1 }, { x: 2, y: 0 },
+    expect(game.describePath("20", "02")).toEqual([
+      "02", "11", "20",
     ]);
   });
 
   test("calculate a valid diagonal line, positive slope", () => {
-    expect(game.describePath({ x: 0, y: 0 }, { x: 3, y: 3 })).toEqual([
-      { x: 0, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }
+    expect(game.describePath("00", "33")).toEqual([
+      "00", "11", "22", "33",
     ]);
-    expect(game.describePath({ x: 1, y: 0 }, { x: 3, y: 2 })).toEqual([
-      { x: 1, y: 0 }, { x: 2, y: 1 }, { x: 3, y: 2 },
+    expect(game.describePath("10", "32")).toEqual([
+      "10", "21", "32",
     ]);
-    expect(game.describePath({ x: 2, y: 3 }, { x: 1, y: 2 })).toEqual([
-      { x: 1, y: 2 }, { x: 2, y: 3 },
+    expect(game.describePath("23", "12")).toEqual([
+      "12", "23",
     ]);
   });
 
   test("determine whether point has valid adjacent slopes", () => {
     // corner has no more valid adjacent nodes
-    game.forbiddenNodes.push({ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 0, y: 1 });
-    expect(game.hasValidAdjacentNodes({ x: 0, y: 0 })).toEqual(false);
+    game.forbiddenNodes.push("10", "11", "01");
+    expect(game.hasValidAdjacentNodes("00")).toEqual(false);
     game.reset();
 
     // corner only has one valid adjacent node left to use
-    game.forbiddenNodes.push({ x: 1, y: 0 }, { x: 1, y: 1 }, );
-    expect(game.hasValidAdjacentNodes({ x: 0, y: 0 })).toEqual(true);
+    game.forbiddenNodes.push("10", "11");
+    expect(game.hasValidAdjacentNodes("00")).toEqual(true);
 
-    game.forbiddenNodes.push({ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 2 }, { x: 1, y: 3 });
-    expect(game.hasValidAdjacentNodes({ x: 0, y: 2 })).toEqual(true);
+    game.forbiddenNodes.push("01", "11", "12", "13");
+    expect(game.hasValidAdjacentNodes("02")).toEqual(true);
 
   });
 
   test("calculate slope", () => {
-    expect(game.calculateSlope({ x: 0, y: 0 }, { x: 1, y: 1 })).toEqual(1);
-    expect(game.calculateSlope({ x: 0, y: 0 }, { x: 1, y: 2 })).toEqual(2);
-    expect(game.calculateSlope({ x: 2, y: 3 }, { x: 5, y: 13 })).toEqual(10 / 3);
+    expect(game.calculateSlope("00", "11", { x: 0, y: 0 }, { x: 1, y: 1 })).toEqual(1);
+    expect(game.calculateSlope("00", "12", { x: 0, y: 0 }, { x: 1, y: 2 })).toEqual(2);
   });
 
   test("determine if two points are octilinear to eachother", () => {
-    expect(game.isOctilinear({ x: 0, y: 0 }, { x: 1, y: 1 })).toBe(true);
-    expect(game.isOctilinear({ x: 0, y: 0 }, { x: 2, y: 2 })).toBe(true);
-    expect(game.isOctilinear({ x: 0, y: 3 }, { x: 3, y: 0 })).toBe(true);
-    expect(game.isOctilinear({ x: 1, y: 2 }, { x: 0, y: 1 })).toBe(true);
+    expect(game.isOctilinear("00", "11")).toBe(true);
+    expect(game.isOctilinear("00", "22")).toBe(true);
+    expect(game.isOctilinear("03", "30")).toBe(true);
+    expect(game.isOctilinear("12", "01")).toBe(true);
 
-    expect(game.isOctilinear({ x: 0, y: 0 }, { x: 2, y: 3 })).toBe(false);
-    expect(game.isOctilinear({ x: 1, y: 2 }, { x: 3, y: 1 })).toBe(false);
+    expect(game.isOctilinear("00", "23")).toBe(false);
+    expect(game.isOctilinear("12", "31")).toBe(false);
 
   });
 
   test("any first move should be valid", () => {
-    expect(game.isValidStartNode({ x: 0, y: 0 })).toEqual(true);
+    expect(game.isValidStartNode("00")).toEqual(true);
   });
 
 
