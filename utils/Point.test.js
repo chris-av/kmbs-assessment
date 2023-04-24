532e1fb707c16598e20f67783e6928dfd0485d44
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-04-23 19:33:48 -0700

    add test

--- utils/Point.test.js
+++ utils/Point.test.js
@@ -9,4 +9,8 @@ describe("test methods of Point", () => {
     const point = new Point(2, 4);
     expect(point.objectify()).toEqual({ x: 2, y: 4 });
   });
+  test("translate a string to its object equivalent", () => {
+    expect(Point.objectifyFromString("23")).toEqual({ x: 2, y: 3 });
+    expect(Point.objectifyFromString("11")).toEqual({ x: 1, y: 1 });
+  });
 });
