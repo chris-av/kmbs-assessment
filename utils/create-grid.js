2c2792d0f0da0831e1fcbd3619a236daa020838f
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-04-23 19:30:24 -0700

    use strings instead of objs

--- utils/create-grid.js
+++ utils/create-grid.js
@@ -1,8 +1,10 @@
+const Point = require('./Point');
 module.exports = function(x_n, y_n) {
   let board = [];
   for (let i = 0; i < x_n; i++) {
     for (let j = 0; j < y_n; j++) {
-      board.push({ x: i, y: j });
+      const point = new Point(i, j).stringify();
+      board.push(point);
     }
   }
   return board;
